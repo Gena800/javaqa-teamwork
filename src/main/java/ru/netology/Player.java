840a85bea05aa812f7e6672f3f558ab2ba9da3d2
s_ishukov <s_ishukov@bk.ru>
2023-03-25 18:44:59 +0500

    fixed #1 (Исправлен метод installGame), fixed #2 (В метод play добавлено исключение RunTimeException для отрицательного значения времени), fixed #3 (Исправлен метод sumGenre), fixed #4 (В метод play добавлено исключение RunTimeException при попытке сыграть в неустановленную игру), fixed #5 (Переписан метод  mostPlayerByGenre).

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -6,9 +6,11 @@ import java.util.Map;
 public class Player {
     private String name;
 
-    /** информация о том, в какую игру сколько часов было сыграно
-    ключ - игра
-    значение - суммарное количество часов игры в эту игру */
+    /**
+     * информация о том, в какую игру сколько часов было сыграно
+     * ключ - игра
+     * значение - суммарное количество часов игры в эту игру
+     */
     private Map<Game, Integer> playedTime = new HashMap<>();
 
     public Player(String name) {
@@ -19,22 +21,26 @@ public class Player {
         return name;
     }
 
-    /** добавление игры игроку
-    если игра уже была, никаких изменений происходить не должно */
+    /**
+     * добавление игры игроку
+     * если игра уже была, никаких изменений происходить не должно
+     */
     public void installGame(Game game) {
-        if (playedTime.containsKey(game)){
+        if (playedTime.containsKey(game)) {
             return;
         }
         playedTime.put(game, 0);
     }
 
-    /** игрок играет в игру game на протяжении hours часов
-    об этом нужно сообщить объекту-каталогу игр, откуда была установлена игра
-    также надо обновить значения в мапе игрока, добавив проигранное количество часов
-    возвращает суммарное количество часов, проигранное в эту игру.
-    если игра не была установлена, то надо выкидывать RuntimeException */
+    /**
+     * игрок играет в игру game на протяжении hours часов
+     * об этом нужно сообщить объекту-каталогу игр, откуда была установлена игра
+     * также надо обновить значения в мапе игрока, добавив проигранное количество часов
+     * возвращает суммарное количество часов, проигранное в эту игру.
+     * если игра не была установлена, то надо выкидывать RuntimeException
+     */
     public int play(Game game, int hours) {
-        if (playedTime.containsKey(game)){
+        if (playedTime.containsKey(game)) {
             if (hours >= 0) {
                 game.getStore().addPlayTime(name, hours);
                 playedTime.put(game, playedTime.get(game) + hours);
@@ -45,8 +51,10 @@ public class Player {
         throw new RuntimeException("Игра не установлена");
     }
 
-    /** Метод принимает жанр игры (одно из полей объекта игры) и
-     суммирует время, проигранное во все игры этого жанра этим игроком */
+    /**
+     * Метод принимает жанр игры (одно из полей объекта игры) и
+     * суммирует время, проигранное во все игры этого жанра этим игроком
+     */
     public int sumGenre(String genre) {
         int sum = 0;
         for (Game game : playedTime.keySet()) {
@@ -57,13 +65,15 @@ public class Player {
         return sum;
     }
 
-    /** Метод принимает жанр и возвращает игру этого жанра, в которую играли больше всего
-     Если в игры этого жанра не играли, возвращается null */
+    /**
+     * Метод принимает жанр и возвращает игру этого жанра, в которую играли больше всего
+     * Если в игры этого жанра не играли, возвращается null
+     */
     public Game mostPlayerByGenre(String genre) {
         int mostTime = 0;
         Game mostPlayedGame = null;
-        for (Game game : playedTime.keySet()){
-            if (game.getGenre().equals(genre) && playedTime.get(game) > mostTime){
+        for (Game game : playedTime.keySet()) {
+            if (game.getGenre().equals(genre) && playedTime.get(game) > mostTime) {
                 mostTime = playedTime.get(game);
                 mostPlayedGame = game;
             }
